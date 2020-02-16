e083f6fc91ae32974975059a0f576c84785f3f27
vyoung98 <val.young98@gmail.com>
2020-02-16 10:16:24 -0500

    views/models on students

--- students/models.py
+++ students/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django import forms
 
-# Create your models here.
+class StudentSignupForm(forms.Form):
+    phone_num = forms.CharField(required=True)
+    classes = forms.CharField(required=True, widget=forms.Textarea)
--- students/views.py
+++ students/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from polls.forms import SuggestionsForm
 
 
 def index(request):
