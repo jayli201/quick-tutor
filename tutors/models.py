3902be8b627dae99bcde6c67af7a5ccdf4393bd9
Jasmin Li <jl6ww@virginia.edu>
2020-02-19 22:07:20 -0500

    added dropdowns

--- tutors/models.py
+++ tutors/models.py
@@ -1,12 +1,25 @@
 from django.db import models
 from django import forms
 
+SUBJECT_CHOICES = (
+    ('science','science'),
+    ('math', 'math'),
+    ('english','english'),
+)
+
+PAY_CHOICES = (
+    ('venmo', 'venmo'),
+    ('cash', 'cash'),
+    ('paypal', 'paypal'),
+)
+
+
 class TutorSignup(models.Model):
-    phone_number = models.CharField(max_length=12, default=0)
-    classes = models.TextField(max_length=120, default=0)
-    subjects = models.TextField(max_length=120, default=0)
-    pay = models.CharField(max_length=12, default=0)
-    payment_method = models.TextField(max_length=120, default=0)
+    phone_number = models.CharField(max_length=12, default="")
+    classes = models.TextField(max_length=120, default="")
+    subjects = models.CharField(max_length=120, choices=SUBJECT_CHOICES, default="")
+    pay = models.CharField(max_length=12, default="")
+    payment_method = models.CharField(max_length=120, choices=PAY_CHOICES, default="")
 
     def __str__(self):
         if self.phone_number==None:
