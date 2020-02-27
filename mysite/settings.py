6a87f7b4e14c008b34d964f52d2c263a67928b73
Julian Cornejo Castro <juliancorn64@gmail.com>
2020-02-27 16:18:00 -0500

    added host

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'vz19e=64((eeiwc*fw)v%0!)_@-sbdekfboi@g=wj9x1ih-3ff'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['studybuddyqt.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
