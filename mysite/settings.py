8b568933a64a6cf6e58b2585a49f8b9553f3c1a9
Julian Cornejo Castro <juliancorn64@gmail.com>
2020-02-27 16:30:02 -0500

    fixed heroku issues #1

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'vz19e=64((eeiwc*fw)v%0!)_@-sbdekfboi@g=wj9x1ih-3ff'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['studybuddyqt.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['studybuddyqt.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
