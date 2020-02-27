07298cce5d3ec2e31d61c32ae3b56516f24fc2fd
walterdarwin2 <59974270+walterdarwin2@users.noreply.github.com>
2020-02-27 16:15:42 -0500

    basic student signup test case

--- students/tests.py
+++ students/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from students.models import StudentSignup
 
-# Create your tests here.
+class BasicTestCase(TestCase):
+    def test_1eq1(self):
+        self.assertTrue(1 == 1)
+
+class StudentSignUpTestCase(TestCase):
+    def setUp(self):
+        StudentSignup.objects.create(phone_number="1234567890", classes="CS 3240")
+
+    def test_students(self):
+        student = StudentSignup.objects.get(phone_number="1234567890")
+        self.assertEqual(student.classes, "CS 3240")
