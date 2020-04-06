934a5266707fdae2f27739da1c90ab2c9b2c5919
Noah Basile <noahbasile@Patricks-MacBook-Pro-2.local>
2020-04-05 20:14:24 -0400

    added logout and switch profile button

--- students/views.py
+++ students/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, JsonResponse
 from django.views import generic
 from django.shortcuts import render
+from django.contrib.auth import logout
 from .models import StudentSignup
 from tutors.models import TutorSignup
 from django.contrib import messages
@@ -34,6 +35,10 @@ def home(request):
         final_coords = eval(coords)
         tutor_coords.append(final_coords)
     return render(request, 'students/home.html', {'ACCESS_TOKEN': ACCESS_TOKEN, 'tutors_list': tutor_coords}) 
+
+def logoutview(request):
+    logout(request)
+    return redirect('students:landing')
 
 def edit_form(request):
     if request.method == 'POST':
@@ -76,6 +81,16 @@ def choose_signup(request):
 class ProfileView(generic.ListView):
     template_name = 'students/profile.html'
     context_object_name = 'profile_list'
+
+    def get_context_data(self, **kwargs):
+        try:
+            context = super().get_context_data(**kwargs)
+            user = TutorSignup.objects.get(user=self.request.user)
+            context['hasother'] = 'yes'
+            return context
+        except:
+            context['hasother'] = 'no'
+            return context
 
     def get_queryset(self):
         return StudentSignup.objects.filter(user=self.request.user)
