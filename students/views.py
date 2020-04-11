9fac733aa121dd547f4eff061c90e8479f9600c3
Noah Basile <noahbasile@Patricks-MacBook-Pro-2.local>
2020-04-11 01:43:17 -0400

    fixed login and edit defects

--- students/views.py
+++ students/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 from django.views import generic
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import StudentSignup
 from tutors.models import TutorSignup, Request
 from django.contrib import messages
@@ -10,7 +12,11 @@ from django.shortcuts import redirect
 from django.contrib.auth.models import User
 
 def landing(request):
-    return render(request, 'students/landing.html') 
+    if request.user.is_authenticated:
+        signed_in = 'yes'
+    else:
+        signed_in = "no"
+    return render(request, 'students/landing.html', {"sign_in":signed_in}) 
     #small change
 
 # getting the secret access token from .env file
@@ -19,6 +25,7 @@ load_dotenv()
 import os
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
 
+@login_required(login_url='students:landing')
 def home(request):
     tutors = TutorSignup.objects.all()
     active_tutors = []
@@ -41,6 +48,7 @@ def logoutview(request):
     logout(request)
     return redirect('students:landing')
 
+@login_required(login_url='students:landing')
 def edit_form(request):
     if request.method == 'POST':
         phone = request.POST['phone_number']
@@ -54,6 +62,7 @@ def edit_form(request):
         form = StudentSignupForm()
     return render(request, 'students/edit.html', {'form': form})
 
+@login_required(login_url='students:landing')
 def signup_form(request):
     if request.method == 'POST':
         form = StudentSignupForm(request.POST)
@@ -72,17 +81,22 @@ def signup_form(request):
     else:
         form = StudentSignupForm()
     return render(request, 'students/signup.html', {'form': form}) 
-    
+
+@login_required(login_url='students:landing')    
 def search(request):
     return render(request, 'students/search.html')
 
+@login_required(login_url='students:landing')
 def choose_signup(request):
     return render(request, 'students/choose.html')
 
+@login_required(login_url='students:landing')
 def sign_in_as(request):
     return render(request, 'students/sign_in_as.html')
 
-class ProfileView(generic.ListView):
+class ProfileView(LoginRequiredMixin, generic.ListView):
+    login_url = 'students:landing'
+    redirect_field_name = 'redirect_to'
     template_name = 'students/profile.html'
     context_object_name = 'profile_list'
 
@@ -104,6 +118,7 @@ class ProfileView(generic.ListView):
         except:
             return StudentSignup.objects.filter(user=self.request.user)
 
+@login_required(login_url='students:landing')
 class TutorProfileView(generic.ListView):
     template_name = 'tutors/profile.html'
     context_object_name = 'profile_list'
@@ -111,7 +126,7 @@ class TutorProfileView(generic.ListView):
     def get_queryset(self):
         return StudentSignup.objects.filter(user=self.request.user)
 
-
+@login_required(login_url='students:landing')
 def request_view(request):
     template_name = 'students/requests.html'
     context_object_name = 'requests_list'
@@ -120,6 +135,7 @@ def request_view(request):
 
     return render(request, 'students/requests.html', {'requests_list': requests}) 
 
+@login_required(login_url='students:landing')
 def request_close(request):
     if request.method == 'POST':
         request_id = int(request.POST['request_id'])
