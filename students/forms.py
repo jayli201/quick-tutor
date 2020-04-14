5ac78574c630ded2c3c034350f7532a24ffbe97d
vyoung98 <val.young98@gmail.com>
2020-04-13 23:50:16 -0400

    added crispy-forms and form required message

--- students/forms.py
+++ students/forms.py
@@ -7,3 +7,10 @@ class StudentSignupForm(forms.ModelForm):
     class Meta:
         model = StudentSignup
         fields = ('phone_number', 'classes')
+        required = ('phone_number', 'classes')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.Meta.required:
+            self.fields[field].required = True
--- tutors/forms.py
+++ tutors/forms.py
@@ -7,4 +7,10 @@ class TutorSignupForm(forms.ModelForm):
     class Meta:
         model = TutorSignup
         fields = ('phone_number', 'classes', 'subjects', 'pay', 'payment_method')
+        required = ('phone_number', 'classes', 'subjects', 'pay', 'payment_method')
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.Meta.required:
+            self.fields[field].required = True
